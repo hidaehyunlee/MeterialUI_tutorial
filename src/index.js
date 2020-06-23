fd99ff7a366efa518c6dd62646a36b0ae283283f
leehi0210 <leehi0210@gmail.com>
2020-06-23 10:27:00 +0900

    Add MuiThemeProvider and createMuiTheme for apply 'NEXON Lv2 Gothic Bold' font

--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+	typography: {
+	  fontFamily: [
+		'NEXON Lv2 Gothic Bold',
+		'sans-serif',
+		'Nunito',
+		'Roboto',
+		'"Helvetica Neue"',
+		'Arial',
+	  ].join(','),
+	}
+});
 
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
+	<MuiThemeProvider theme={theme}>
+    	<App />
+	</MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
